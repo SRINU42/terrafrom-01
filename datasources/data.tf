f655edb608592ad882f40c03cef7da2f1d2cc6c7
SRINU42 <srinu.karnam01@gmail.com>
2025-01-24 17:56:06 +0530

    ami

--- datasources/data.tf
+++ datasources/data.tf
@@ -19,3 +19,16 @@ data "aws_security_group" "selected" {
 output "sgid" {
     value = data.aws_security_group.selected.id
 }
+
+data "aws_ami" "example" {
+  executable_users = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["self"]
+
+  }
+
+  output "ami" {
+    value = data.aws.example.id
+  }
+
