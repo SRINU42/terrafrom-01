94bb3f396bee34cf9548072417d9809f0c5eb0a4
SRINU42 <srinu.karnam01@gmail.com>
2025-01-24 23:37:46 +0530

    module1

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amiid.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
 
   tags = {
     Name = "sample"
