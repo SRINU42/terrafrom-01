75bcc4e365ef64108dc6f6867c012618dc1858e8
SRINU42 <srinu.karnam01@gmail.com>
2025-01-25 15:23:43 +0530

    provisioner

---
+++ provisioner/main.tf
@@ -0,0 +1,63 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amiid.id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = var.name
+  }
+}
+
+resource "aws_instance" "web" {
+  
+  connection {
+    type     = "ssh"
+    user     = "Centos"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo labauto ansible" ,
+      "ansible-pull -i localhost, -U https://github.com/SRINU42/roboansible.git main.yml -e env=dev -e role_name=frontend"
+    ]
+  }
+}
+
+
+
+data "aws_ami" "amiid" {
+  owners           = ["973714476881"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+
+  }
+
+resource "aws_security_group" "sg" {
+  name        = var.name
+  description = "Allow TLS inbound traffic"
+ 
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.name
+  }
+}
+
+variable "name" {}
+
