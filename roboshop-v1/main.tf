344ad3fff76bc5b3e598bef292005611c36a7e1c
SRINU42 <srinu.karnam01@gmail.com>
2025-01-28 18:21:07 +0530

    depends

--- roboshop-v1/main.tf
+++ roboshop-v1/main.tf
@@ -1,49 +1,22 @@
 module "frontend" {
+    for_each = var.instances
     source = "./ec2"
-    name = "frontend"
+    name = each.key
 }
 
-module "mongodb" {
-    source = "./ec2"
-    name = "mogodb"
-}
+variable "instances" {
+    default = {
 
-module "mysql" {
-    source = "./ec2"
-    name = "mysql"
-}
-
-module "redis" {
-    source = "./ec2"
-    name = "redis"
-}
-
-module "catalogue" {
-    source = "./ec2"
-    name = "catalogue"
-}
-
-module "user" {
-    source = "./ec2"
-    name = "user"
-}
-
-module "shipping" {
-    source = "./ec2"
-    name = "shipping"
-}
-
-module "rabbitmq" {
-    source = "./ec2"
-    name = "rabbitmq"
-}
-
-module "payment" {
-    source = "./ec2"
-    name = "payment"
-}
-
-module "dispatch" {
-    source = "./ec2"
-    name = "dispatch"
+        frontend = {}
+        mongodb = {}
+        catalogue = {}
+        redis  = {}
+        user = {}
+        cart = {}
+        mysql = {}
+        shipping = {}
+        rabbitmq = {}
+        payment = {}
+        dispatch = {} 
+    }
 }
